984e2d2ad955643c9a8cecf3496ed6e07ec8d9d6
kamishi-44 <kamishioiri1016@gmail.com>
2023-11-07 23:53:38 +0900

    :tada: Spring Boot のプロジェクトファイルを追加する

---
+++ src/main/kotlin/com/example/awsdeploysample/AwsDeploySampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.awsdeploysample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsDeploySampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<AwsDeploySampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/awsdeploysample/AwsDeploySampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.awsdeploysample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwsDeploySampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
